48f537ed33315ecaa22f28aa2f86a00b765d5f7c
Mark Jansen <mark.jansen@reactos.org>
2020-11-01 16:51:42 +0100

    Dump all test results for a suite+platform in a graph

--- www/www.reactos.org/testman/detail.php
+++ www/www.reactos.org/testman/detail.php
@@ -11,14 +11,18 @@
 	require_once(ROOT_PATH . "../www.reactos.org_config/testman-connect.php");
 	require_once("utils.inc.php");
 	require_once("languages.inc.php");
+	require_once("autoload.inc.php");
 	require_once(ROOT_PATH . "rosweb/exceptions.php");
 	require_once(ROOT_PATH . "rosweb/rosweb.php");
+	require_once(ROOT_PATH . "rosweb/gitinfo.php");
 
 	//$rw = new RosWeb($supported_languages);
 	$rw = new RosWeb();
 	$lang = $rw->getLanguage();
 	require_once(ROOT_PATH . "rosweb/lang/$lang.inc.php");
 	require_once("lang/$lang.inc.php");
+
+	$gi = new GitInfo();
 
 	try
 	{
@@ -34,7 +38,7 @@
 
 		// Get information about this result.
 		$stmt = $dbh->prepare(
-			"SELECT UNCOMPRESS(l.log) AS log, e.status, e.count, e.failures, e.skipped, e.todo, e.time, s.module, s.test, UNIX_TIMESTAMP(r.timestamp) AS timestamp, r.revision, r.platform, src.name, r.comment " .
+			"SELECT UNCOMPRESS(l.log) AS log, e.status, e.count, e.failures, e.skipped, e.todo, e.time, s.module, s.test, UNIX_TIMESTAMP(r.timestamp) AS timestamp, r.revision, r.platform, src.name, r.comment, e.suite_id " .
 			"FROM winetest_results e " .
 			"JOIN winetest_logs l ON e.id = l.id " .
 			"JOIN winetest_suites s ON e.suite_id = s.id " .
@@ -57,6 +61,9 @@
 
 		$log = preg_replace($pattern_core, $replacement_core, htmlspecialchars($row["log"]));
 		$log = preg_replace_callback($pattern_test, "file_callback", $log);
+
+		$reader = new WineTest_Reader();
+		$history = $reader->getTestHistory($row['suite_id'], $row['platform']);
 	}
 	catch (ErrorMessageException $e)
 	{
@@ -105,6 +112,8 @@
 	<title><?php echo $testman_langres["detail_title"]; ?></title>
 	<?php $rw->printHead(); ?>
 	<link rel="stylesheet" type="text/css" href="css/detail.css">
+	<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+	<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0"></script>
 </head>
 <body>
 
@@ -125,6 +134,145 @@
 				<div class="col"><strong><?php echo $testman_langres["skippedtests"]; ?>:</strong> <?php echo $row["skipped"]; ?></div>
 				<div class="col"><strong><?php echo $testman_langres["todotests"]; ?>:</strong> <?php echo $row["todo"]; ?></div>
 				<div class="col"><strong><?php echo $testman_langres["timetest"]; ?>:</strong> <?php echo $row["time"]; ?>s</div>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<canvas id="myChart" height="200px"></canvas>
+				<script>
+				<?php
+					$succeeded_tests = array();
+					$failed_tests = array();
+					$test_times = array();
+					$revision_list = array();
+					foreach ($history as $item)
+					{
+						$count = (int)$item['count'];
+						$skipped = (int)$item['skipped'];
+						$failed = (int)$item['failures'];
+						$succeeded_tests[] = ($count - $failed);
+						$failed_tests[] = $failed;
+						$test_times[] = $item['time'];
+						$revision_list[] = $gi->getShortHash($item['revision']);
+					}
+				?>
+				var ctx = document.getElementById('myChart').getContext('2d');
+				var chart = new Chart(ctx,
+				{
+					type: 'bar',
+					data: {
+						datasets: [{
+							label: 'Failed',
+							data: <?php echo json_encode($failed_tests); ?>,
+							backgroundColor: "rgba(255, 99, 132, 0.2)",
+							borderColor: "rgb(255, 99, 132)",
+							borderWidth: 1,
+							barPercentage: 1,
+							categoryPercentage: 1,
+							yAxisID: 'bar-y-axis',
+							order: 2
+						}, {
+							label: 'Succeeded',
+							data: <?php echo json_encode($succeeded_tests); ?>,
+							backgroundColor: "rgba(54, 162, 235, 0.2)",
+							borderColor: "rgb(54, 162, 235)",
+							borderWidth: 1,
+							barPercentage: 1,
+							categoryPercentage: 1,
+							yAxisID: 'bar-y-axis',
+							order: 1
+						}, {
+							label: 'Test time',
+							data: <?php echo json_encode($test_times); ?>,
+							order: 3,
+							borderColor: "rgba(153, 102, 255, 1)",
+							yAxisID: 'line-y-axis',
+							type: 'line',
+							fill: false,
+							lineTension: 0,
+							datalabels: {
+								offset: '10',
+								align: 'right',
+								backgroundColor: 'white',
+								formatter: function(value, ctx) {
+									if (ctx.dataIndex == 0) {
+										return value;
+									} else {
+										let prev = ctx.dataset.data[ctx.dataIndex - 1];
+										if (prev < value) {
+											return "+" + (value-prev).toFixed(1);
+										} else if (prev > value) {
+											return "-" + (prev-value).toFixed(1);
+										} else {
+											return ".";
+										}
+									}
+								},
+							}
+						}],
+						labels: <?php echo json_encode($revision_list); ?>
+					},
+
+					options: {
+						maintainAspectRatio: false,
+						plugins: {
+							datalabels: {
+								align: 'end',
+								color: function(ctx) {
+									return ctx.dataset.borderColor;
+								},
+								formatter: function(value, ctx) {
+									if (ctx.dataIndex == 0) {
+										return value;
+									} else {
+										let prev = ctx.dataset.data[ctx.dataIndex - 1];
+										if (prev < value) {
+											return "+" + (value-prev);
+										} else if (prev > value) {
+											return "-" + (prev-value);
+										} else {
+											return ".";
+										}
+									}
+								},
+							}
+						},
+						elements: {
+							line: {
+								tension: 0	// disable bezier curves
+							}
+						},
+						scales: {
+							yAxes: [{
+								id: 'bar-y-axis',
+								type: 'linear',
+								showLine: true,
+								stacked: true,
+								ticks: {
+									beginAtZero: true,
+								},
+							}, {
+								id: 'line-y-axis',
+								type: 'linear',
+								ticks: {
+									beginAtZero: false,
+									fontColor: "rgba(153, 102, 255, 1)"
+								},
+							}],
+							xAxes: [
+								{ stacked: true }
+							]
+						}
+						/*animation: {
+							duration: 0 // general animation time
+						},
+						hover: {
+							animationDuration: 0 // duration of animations when hovering an item
+						},
+						responsiveAnimationDuration: 0 // animation duration after a resize*/
+					}
+				});
+				</script>
 			</td>
 		</tr>
 		<?php
